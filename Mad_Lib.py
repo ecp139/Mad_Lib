905affdbab504ba6216c8c4c7a2fe1074d9b5290
Esra Petursson <ecp139@gmail.com>
2017-08-16 15:54:41 -0400

    Mad Lib Code

---
+++ Mad_Lib.py
@@ -0,0 +1,8 @@
+noun = raw_input("Enter a noun ")
+verb = raw_input("Enter a verb ")
+adjective = raw_input("Enter an adjective ")
+
+noun = noun + ","
+adjective = adjective + "."
+
+print "The dog ran up to a", noun, "and he", verb, "his tail", adjective
